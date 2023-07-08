b5e34a5a9c244fe8437fcc15defddce17e5af5bc
karayilann <muhammethamitkarayilan@gmail.com>
2023-07-09 02:35:11 +0300

    Project Created

---
+++ Assets/Scripts/Full_Screen.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Full_Screen : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+        Vector2 tempScale = transform.localScale;
+
+        float spriteGenislik = renderer.size.x;
+        
+        // çarpma simgesinin solu sadece orjinden alt ve üst arasýndaki mesafeyi verir. Yüksekliði bulmak için 2 ile çarptýk.
+        float ekranYukseklik = Camera.main.orthographicSize * 2.0f;  
+    }
+
+
+
+
+    void Update()
+    {
+        
+    }
+}
